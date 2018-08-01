5aaee34f362e5cb1c74d46fd301518ad4fe8594c
王渊博 <developer2@easynew.jp>
2018-08-01 12:56:26 +0900

    Initial Commit

---
+++ MMMapDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MMMapDemo
+//
+//  Created by 王淵博 on 2018/08/01.
+//  Copyright © 2018年 王淵博. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
